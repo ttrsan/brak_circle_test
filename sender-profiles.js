309be0ea6e43fce742418ce121bc474c0b208c6b
aaa <aaa@aaa>
2026-05-16 15:20:35 +0900

    a

--- sender-profiles.js
+++ sender-profiles.js
@@ -22,5 +22,37 @@ const senderProfiles = {
     icon: "assets/icons/abydos/shiroko.png",
     avatarText: "シ",
     useCustomName: false
+  },
+  hoshino: {
+    name: "ホシノ",
+    title: "対策委員会",
+    plateClass: "blue",
+    icon: "assets/icons/abydos/hoshino.png",
+    avatarText: "ホ",
+    useCustomName: false
+  },
+  nonomi: {
+    name: "ノノミ",
+    title: "対策委員会",
+    plateClass: "blue",
+    icon: "assets/icons/abydos/nonomi.png",
+    avatarText: "ノ",
+    useCustomName: false
+  },
+  serika: {
+    name: "セリカ",
+    title: "対策委員会",
+    plateClass: "blue",
+    icon: "assets/icons/abydos/serika.png",
+    avatarText: "セ",
+    useCustomName: false
+  },
+  ayane: {
+    name: "アヤネ",
+    title: "対策委員会",
+    plateClass: "blue",
+    icon: "assets/icons/abydos/ayane.png",
+    avatarText: "ア",
+    useCustomName: false
   }
 };
